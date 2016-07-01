a654d33f5f297f1815d0d0e90a232f422c42ab5e
zzxy72 <keytest@naver.com>
2016-07-01 16:14:30 +0900

    start 0701

---
+++ afproject_2/Assets/Project_Root/Scripts/InGame/PlayerController_2d.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class wt_Boundary 
+{
+	public float xMin, xMax, yMin, yMax;
+}
+
+public class PlayerController_2d : MonoBehaviour
+{
+	public float m_speed = 1.5f;
+	public wt_Boundary m_boundary;
+
+    Rigidbody2D _Rigidbody2D;
+    Transform _Trans;
+    void Start()
+    {
+        _Rigidbody2D = GetComponent<Rigidbody2D>();
+        _Trans = this.transform;
+    }
+	void Update ()
+	{
+		
+        if (Input.GetMouseButtonDown(0))
+        {
+            _isDrag = true;
+            _oldMousePos = Input.mousePosition;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            _isDrag = false;
+        }
+	}
+    
+    bool _isDrag = false;
+    Vector2 _oldMousePos = Vector2.zero;
+
+    public float moveHorizontal;
+    public float moveVertical;
+    float move_ratioX = 1; // (0.16f * 1.5f);
+//    float move_ratioY = (0.16f * 1.5f);
+    void FixedUpdate ()
+	{
+        if (_Rigidbody2D != null && _Trans != null)
+        {
+            moveHorizontal = Input.GetAxis("Horizontal");
+            //moveVertical =  Input.GetAxis("Vertical");
+
+            if (_isDrag == true)
+            {
+                
+                moveHorizontal = (Input.mousePosition.x - _oldMousePos.x) * move_ratioX;
+                //moveVertical = (Input.mousePosition.y - _oldMousePos.y) * move_ratioY;
+                _oldMousePos = Input.mousePosition;
+            }
+
+            moveVertical = 0; // 좌우만 입력 처리.
+
+            Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+            _Rigidbody2D.velocity = movement * m_speed;
+
+            _Trans.position = new Vector3
+            (
+                Mathf.Clamp(_Trans.position.x, m_boundary.xMin, m_boundary.xMax),
+                Mathf.Clamp(_Trans.position.y, m_boundary.yMin, m_boundary.yMax),
+                0.0f
+            );
+        }
+	}
+   
+}
---
+++ afproject_2/Assets/Project_Root/Scripts/InGame/Player_Bolt.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Bolt : MonoBehaviour {
+    public bool _AutoStart = true;
+    public float _StartDelay = 0f;
+    public float speed;
+    public bool _IsPlayMove = false;
+
+    Rigidbody2D _Rigidbody2D;
+
+    void Start()
+    {
+        _Rigidbody2D = GetComponent<Rigidbody2D>();
+
+        //if (_isLocalMover == false)
+        //{
+        //    if (_isMulti == false)
+        //    {
+        //        Vector2 movement = new Vector2(1, 0);
+        //        _Rigidbody2D.velocity = movement * speed;
+        //    }
+        //    else
+        //    {
+        //        Vector2 movement = new Vector2(_speedX, _speedY);
+        //        _Rigidbody2D.velocity = movement;
+        //    }
+        //}
+        //else
+        //{
+
+        //}
+
+        if (_AutoStart == true)
+        {
+            PlayMove();
+        }
+    }
+    public void PlayMove()
+    {
+        Invoke("SetPlayMove", _StartDelay);
+    }
+    void SetPlayMove()
+    {
+        _IsPlayMove = true;
+    }
+
+    void Update()
+    {
+        if (_IsPlayMove == true)
+        {
+            //Vector3 move_dir = new Vector3(1, 0, 0);
+            //transform.localPosition = transform.localPosition + (move_dir * speed * Time.deltaTime);
+
+            _Rigidbody2D.velocity = (new Vector2(0, 1) * speed);
+        }
+    }
+}
